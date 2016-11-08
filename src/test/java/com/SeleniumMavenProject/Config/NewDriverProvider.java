48ac1319816ddc0bfaa4df164f01138136c632fb
Shashank Gupta <shashank.sj_2006@yahoo.co.in>
2016-11-08 15:43:04 +0530

    Update NewDriverProvider.java

--- src/test/java/com/SeleniumMavenProject/Config/NewDriverProvider.java
+++ src/test/java/com/SeleniumMavenProject/Config/NewDriverProvider.java
@@ -1,19 +1,14 @@
 package com.SeleniumMavenProject.Config;
 
-import java.io.File;
 import java.net.URL;
 import java.util.concurrent.TimeUnit;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.chrome.ChromeDriverService;
 import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.edge.EdgeDriver;
-import org.openqa.selenium.edge.EdgeDriverService;
 import org.openqa.selenium.firefox.FirefoxDriver;
-import org.openqa.selenium.firefox.GeckoDriverService;
 import org.openqa.selenium.ie.InternetExplorerDriver;
-import org.openqa.selenium.ie.InternetExplorerDriverService;
 import org.openqa.selenium.remote.DesiredCapabilities;
 import org.openqa.selenium.remote.RemoteWebDriver;
 import org.openqa.selenium.safari.SafariDriver;
@@ -22,9 +17,6 @@ import com.SeleniumMavenProject.Common.CustomLogger;
 
 public class NewDriverProvider {
 	private static WebDriver driver;
-	private static String DRIVER_DIR = System.getProperty("user.dir")
-			+ File.separator + "src" + File.separator + "resources"
-			+ File.separator + "Driver";
 
 	private enum Browser {
 		CHROME, FIREFOX, IE, EDGE, REMOTE, SAFARI
@@ -34,25 +26,17 @@ public class NewDriverProvider {
 		return driver;
 	}
 
-	public static WebDriver createWebDriverInstance(String browser) {
-		Browser b = Browser.valueOf(browser);
+	public static WebDriver createWebDriverInstance() {
+		Browser browser = Browser.valueOf(Configuration.getBrowser());
 
-		switch (b) {
+		switch (browser) {
 			// If browser equals IE set driver property as IEWebDriver instance
 			case IE :
-
-				System.setProperty(
-						InternetExplorerDriverService.IE_DRIVER_EXE_PROPERTY,
-						DRIVER_DIR + File.separator + "IEDriver"
-								+ File.separator + "IEDriverServer.exe");
 
 				driver = new InternetExplorerDriver();
 				break;
 
 			case EDGE :
-
-				System.setProperty(EdgeDriverService.EDGE_DRIVER_EXE_PROPERTY,
-						"");
 
 				driver = new EdgeDriver();
 				break;
@@ -60,10 +44,6 @@ public class NewDriverProvider {
 			// If browser equals FIREFOX set driver property as FirefoxWebDriver
 			// instance
 			case FIREFOX :
-
-				System.setProperty(GeckoDriverService.GECKO_DRIVER_EXE_PROPERTY,
-						DRIVER_DIR + File.separator + "FirefoxDriver"
-								+ File.separator + "geckodriver.exe");
 
 				driver = new FirefoxDriver();
 				break;
@@ -73,12 +53,6 @@ public class NewDriverProvider {
 			case CHROME :
 
 				ChromeOptions chromeOptions = new ChromeOptions();
-
-				System.setProperty(
-						ChromeDriverService.CHROME_DRIVER_EXE_PROPERTY,
-						DRIVER_DIR + File.separator + "ChromeDriver"
-								+ File.separator + "chromedriver.exe");
-
 				chromeOptions.addArguments("--disable-extensions");
 
 				driver = new ChromeDriver(chromeOptions);
@@ -113,6 +87,7 @@ public class NewDriverProvider {
 		}
 
 		driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+		driver.manage().timeouts().setScriptTimeout(10, TimeUnit.SECONDS);
 		driver.manage().deleteAllCookies();
 		CustomLogger.logInfo(String.format(
 				"createWebDriverInstance: {%s} Driver started successfully",
