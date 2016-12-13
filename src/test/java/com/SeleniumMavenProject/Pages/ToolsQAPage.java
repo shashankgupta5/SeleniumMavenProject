98bc17b51e98e57b932b94c8fe26e6f813d2bdaa
Shashank Gupta <shashank.sj_2006@yahoo.co.in>
2016-12-13 16:05:11 +0530

    updates for allure reporting

--- src/test/java/com/SeleniumMavenProject/Pages/ToolsQAPage.java
+++ src/test/java/com/SeleniumMavenProject/Pages/ToolsQAPage.java
@@ -5,13 +5,17 @@ import static org.hamcrest.CoreMatchers.equalTo;
 import static org.hamcrest.CoreMatchers.is;
 import static org.hamcrest.MatcherAssert.assertThat;
 
+import java.io.IOException;
 import java.util.List;
 
 import org.apache.commons.lang3.StringUtils;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
 
 import com.SeleniumMavenProject.Common.CustomLogger;
+
+import ru.yandex.qatools.allure.annotations.Step;
 
 public class ToolsQAPage extends BasePage {
 
@@ -24,22 +28,42 @@ public class ToolsQAPage extends BasePage {
 	@FindBy(xpath = "//*[@id='content']/p[4]/button")
 	private WebElement newBrowserTabBtn;
 
-	public void getPracticeTableData() {
+	@FindBy(tagName = "button")
+	private List<WebElement> btns;
+
+	@FindBy(id = "navbar-container")
+	private WebElement navBar;
+
+	@FindBy(linkText = "details")
+	private List<WebElement> details;
+
+	@FindBy(xpath = "//*[@class='cancel']")
+	private WebElement cancelBtn;
+
+	public ToolsQAPage() {
+		super();
+		PageFactory.initElements(driver, this);
+	}
+
+	@Step("Started getting practice table data")
+	public void getPracticeTableData() throws IOException {
 		assertThat(driver.getTitle(),
 				is("Demo Table for practicing Selenium Automation"));
 
 		StringBuilder builder = new StringBuilder();
-		for (WebElement elm : tableData) {
-			String text = elm.getText();
+		tableData.forEach((e) -> {
+			String text = e.getText();
 			if (StringUtils.isNotBlank(text)) {
 				builder.append(text);
 			}
-		}
+		});
+
 		CustomLogger.logInfo(builder.toString());
 		assertThat("table conent shouldn't null", builder.toString(),
 				containsString("Total 4 buildings"));
 	}
 
+	@Step("Started performing window switching")
 	public void performWindowSwitching() {
 		assertThat(driver.getTitle(),
 				is("Demo Windows for practicing Selenium Automation"));
