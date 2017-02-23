3b3052f6ea8d9789bedb571aac2d29b28e15d51a
Shashank Gupta <shashank.sj_2006@yahoo.co.in>
2017-02-23 19:03:56 +0530

    Update ScreenshotListener.java

--- src/test/java/com/SeleniumMavenProject/Common/ScreenshotListener.java
+++ src/test/java/com/SeleniumMavenProject/Common/ScreenshotListener.java
@@ -7,22 +7,24 @@ import java.time.format.DateTimeFormatter;
 import org.testng.ITestResult;
 import org.testng.TestListenerAdapter;
 
-import com.SeleniumMavenProject.Config.NewDriverProvider;
+import com.SeleniumMavenProject.Config.DriverManager;
 import com.SeleniumMavenProject.WebDriverHelper.Shooter;
 
 public class ScreenshotListener extends TestListenerAdapter {
-	private String SCREENSHOT_DIR = System.getProperty("user.dir")
-			+ File.separator + "results" + File.separator + "failure"
-			+ File.separator;
+	private String SCREENSHOT_DIR = System.getProperty("user.dir") + File.separator + "results" + File.separator
+			+ "failure" + File.separator;
 
 	@Override
 	public void onTestFailure(ITestResult result) {
-		String filePath = String.format("%s%s_%s.png", SCREENSHOT_DIR,
-				result.getMethod().getMethodName(), getTimeStamp());
+		String filePath = String.format("%s%s_%s.png", SCREENSHOT_DIR, result.getMethod().getMethodName(),
+				getTimeStamp());
 
-		Shooter failureScreenshot = new Shooter(
-				NewDriverProvider.getWebDriver());
-		failureScreenshot.captureWebPage(filePath);
+		if (DriverManager.getWebDriver() != null) {
+			Shooter failureScreenshot = new Shooter(DriverManager.getWebDriver());
+			failureScreenshot.captureWebPage(filePath);
+		} else {
+			CustomLogger.logError("onTestFailure: Unable to take screenshot as webdriver is null");
+		}
 	}
 
 	private String getTimeStamp() {
